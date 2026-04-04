89069da582d91b7b626ee83fac5248c33fd0104e
Dan Pudsey <dan@danpudsey.com>
2026-04-04 09:55:32 -0500

    Fixed the borders and layout

--- app/public/wp-content/themes/uncommon-editorial/blocks/testimonials/render.php
+++ app/public/wp-content/themes/uncommon-editorial/blocks/testimonials/render.php
@@ -1,12 +1,10 @@
 <?php
-$heading            = $attributes['heading'] ?? '';
-$wrapper_attributes = get_block_wrapper_attributes( [ 'class' => 'testimonials' ] );
+$heading = $attributes['heading'] ?? '';
+$wrapper_attributes = get_block_wrapper_attributes(['class' => 'testimonials']);
 ?>
 <div <?php echo $wrapper_attributes; ?>>
 
-	<?php if ( $heading ) : ?>
-		<h2 class="testimonials__heading"><?php echo esc_html( $heading ); ?></h2>
-	<?php endif; ?>
+	<h2 class="testimonials__heading">Testimonials</h2>
 
 	<div class="testimonials__runner">
 		<div class="testimonials__grid">
